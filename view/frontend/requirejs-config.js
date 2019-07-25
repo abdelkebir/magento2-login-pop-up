fb452766807d27ec1aa6feb1103e8d624c26dd3a
Abdelkebir ELHARFALI <elharfali.abdelkebir@gmail.com>
2019-07-25 13:46:27 +0100

    first commit

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,10 @@
+var config = {
+    paths: {
+         'loginPopUp': "Godogi_LoginPopUp/js/loginPopUp"
+    },
+    shim: {
+        'loginPopUp': {
+            deps: ['jquery']
+        }
+    }
+}
